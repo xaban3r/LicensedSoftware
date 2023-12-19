75e7ebd34b804c38ee88f6b77cdb699b3a07c161
Aleksey <nstr3@mail.ru>
2023-12-19 16:33:16 +0500

    add main window interface

--- db.py
+++ db.py
@@ -37,3 +37,39 @@ class Database:
         if self.conn:
             self.conn.close()
         print("Connection closed successfully!")
+
+
+# Запросы
+# на заданную дату список подразделений, на компьютерах которых
+# установлено не лицензионное ПО;
+"""
+SELECT DISTINCT D.name_organization_division
+FROM Computers C
+JOIN Division D ON C.computer_division = D.name_organization_division
+JOIN Software S ON C.software = S.software_name
+WHERE AGE(C.date_end, C.date_start) > S.validity_period;
+"""
+
+# список лицензионного ПО, количество лицензий на это ПО (по
+# убыванию) на заданную дату;
+
+"""
+SELECT S.software_name, COUNT(*) AS license_count
+FROM Computers C
+JOIN Software S ON C.software = S.software_name
+WHERE CURRENT_DATE <= C.date_end
+GROUP BY S.software_name
+ORDER BY license_count DESC;
+"""
+
+# список подразделений, количество компьютеров у подразделения (по
+# убыванию) на заданную дату.
+
+"""
+SELECT D.name_organization_division, COUNT(*) AS computer_count
+FROM Computers C
+JOIN Division D ON C.computer_division = D.name_organization_division
+WHERE CURRENT_DATE BETWEEN C.date_start AND C.date_end
+GROUP BY D.name_organization_division
+ORDER BY computer_count DESC;
+"""
--- main.py
+++ main.py
@@ -1,17 +1,54 @@
+import sys
+
 from db import Database
+from PySide2.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+from form import Ui_MainWindow
 
 db = Database()
 db.connect()
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Учёт лицензионного ПО")
+        combobox_style = "QComboBox { font-weight: bold; text-align: center; font-size: 16px; }"
+        self.ui.tables_comboBox.addItems(
+            ["Организации", "Подразделения", "Компьютеры", "ПО", "Продавец", "Адреса"])
+        self.ui.reports_comboBox.addItems(
+            ["Список подразделений с нелицензионным ПО", "Список ПО", "Список подразделений"])
+        self.ui.tables_comboBox.setStyleSheet(combobox_style)
+        self.ui.reports_comboBox.setStyleSheet(combobox_style)
+        self.show()
+
+    def show_table(self, rows):
+        self.ui.tableWidget.setRowCount(len(rows))
+        self.ui.tableWidget.setColumnCount(len(rows[0]))
+
+        # Заполняем таблицу данными
+        for i, row in enumerate(rows):
+            for j, val in enumerate(row):
+                item = QTableWidgetItem(str(val))
+                self.ui.tableWidget.setItem(i, j, item)
+
+
 # Обработчик кнопки
-def on_button_click():
-    query = "SELECT * FROM table"
-    db.execute(query)
-    rows = db.cur.fetchall()
-    # обрабатываем результат запроса
+# def on_button_click():
+#     query = "SELECT * FROM table"
+#     db.execute(query)
+#     rows = db.cur.fetchall()
 
 
-# Закрываем соединение с базой данных при выходе из приложения
 def on_app_exit():
     db.close()
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    widget = MainWindow()
+    widget.show()
+    app.aboutToQuit.connect(on_app_exit)
+    sys.exit(app.exec_())
